2f8fddcfa1a94a7485d4cb1e73a1773f69aa11f4
heinrichmartin <heinrichmartin@hotmail.com>
2018-03-02 01:15:32 +0100

    unit CRUD for #8

--- manager/js/manager.js
+++ manager/js/manager.js
@@ -14,3 +14,84 @@ let apiPublic =
     geobroker.config.apiPublic :
     (apiBase + '/public');
 
+function log (text) {
+    if (text && text.responseJSON) {
+        text = text.responseJSON;
+    }
+    if (typeof text !== 'string') {
+        text = JSON.stringify(text, null, 2);
+    }
+    $('<p></p>').text(text).appendTo('body').get(0).scrollIntoView();
+};
+
+function randomToken () {
+  // https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript/30925561#30925561
+  return (Math.random()*1e64).toString(36);
+};
+
+function unitModel (data) {
+  var self = this;
+  ko.mapping.fromJS(data, {
+    copy: ['id'],
+  }, self);
+  self.post = function () {
+    $.ajax({
+        method: 'PUT',
+        url: apiPrivate + '/units/' + encodeURIComponent(self.id),
+        contentType: 'application/json',
+        processData: false,
+        data: ko.toJSON(self),
+    }).fail(log);
+  };
+  self.random = function () {
+    self.token(randomToken());
+  };
+};
+
+$.get(apiPrivate + '/units').fail(log).done(function (data) {
+  let viewModel = ko.mapping.fromJS(data, {
+    'configuredUnits': {
+      create: function (options) {
+        return new unitModel(options.data);
+      },
+      key: function (data) {
+        return data.id;
+      },
+    },
+  });
+  viewModel.newUnitName = ko.observable();
+  viewModel.addUnit = function () {
+    viewModel.newUnitName().split('\n').forEach(function (name) {
+      let unit = new unitModel({
+        id: randomToken(),
+        name: name,
+        token: randomToken(),
+        units: [],
+        incidentes: [],
+      });
+      viewModel.configuredUnits.push(unit);
+      unit.post();
+    });
+  };
+  viewModel.randomAll = function() {
+    viewModel.configuredUnits().forEach(function (unit) {
+      unit.random();
+    });
+  };
+  viewModel.postAll = function() {
+    viewModel.configuredUnits().forEach(function (unit) {
+      unit.post();
+    });
+  };
+  viewModel.removeUnit = function(unit) {
+    viewModel.configuredUnits.remove(unit);
+    $.ajax({
+        method: 'DELETE',
+        url: apiPrivate + '/units/' + encodeURIComponent(unit.id),
+        processData: false,
+        contentType: 'text/plain',
+        dataType: 'text',
+    }).fail(log);
+  };
+  ko.applyBindings(viewModel);
+});
