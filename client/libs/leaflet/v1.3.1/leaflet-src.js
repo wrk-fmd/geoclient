40c0cd08512ca1ac8ea69ffc1c8fca4db12f0016
heinrichmartin <heinrichmartin@hotmail.com>
2018-04-06 13:50:39 +0200

    workaround conflicting tooltip and popup
    leaflet issues 6122

--- client/libs/leaflet/v1.3.1/leaflet-src.js
+++ client/libs/leaflet/v1.3.1/leaflet-src.js
@@ -1,6 +1,7 @@
 /* @preserve
  * Leaflet 1.3.1+Detached: ba6f97fff8647e724e4dfe66d2ed7da11f908989.ba6f97f, a JS library for interactive maps. http://leafletjs.com
  * (c) 2010-2017 Vladimir Agafonkin, (c) 2010-2011 CloudMade
+ * modified by heinrichmartin: no tooltip on touch
  */
 
 (function (global, factory) {
@@ -10231,7 +10232,7 @@ Layer.include({
 			if (this._tooltip.options.sticky) {
 				events.mousemove = this._moveTooltip;
 			}
-			if (touch) {
+			if (0&&touch) {
 				events.click = this._openTooltip;
 			}
 		} else {
